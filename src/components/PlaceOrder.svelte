2f14ae6ca7662ccd57b4c20d4d3b731ca6f2bd49
Catinon <aurelien.catinon@gmail.com>
2019-07-01 18:16:04 +0200

    Add bindings to orders placing

--- src/components/PlaceOrder.svelte
+++ src/components/PlaceOrder.svelte
@@ -1,15 +1,27 @@
 <script>
-import { placeOrder } from "../services/orders"
+import { placeOrder } from "../services/orders";
+import { Side, OrderType } from "../domain/order";
+
+let side;
+let type;
+let quantity;
+let price;
 
 function submitOrder() {
-    placeOrder("BTC-USD", "L", "B", 10, 6970);
+    placeOrder("BTC-USD", type, side, quantity, price);
 }
-
 </script>
 
 <form on:submit|preventDefault={submitOrder}>
-    <input name="quantity" type="text" />
-    <input name="price" type="text" />
+
+    <select name="type" bind:value="{type}">
+        <option value="M">Market</option>
+        <option value="L">Limit</option>
+    </select>
+
+    <input name="quantity" type="number" min="0" bind:value="{quantity}" />
+    
+    <input name="price" type="number" min="0" bind:value="{price}" />
 
     <input type="submit" />
 </form>
