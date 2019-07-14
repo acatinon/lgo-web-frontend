5a2b21c64aba30499d9dcc21c69b0e074243cd43
aurel <aurelien.catinon@gmail.com>
2019-07-14 17:28:26 +0200

    Update trade list UX

--- src/components/TradeList.svelte
+++ src/components/TradeList.svelte
@@ -1,20 +1,37 @@
 <script>
   import { trades } from "../stores/trades";
   import Date from "./Date.svelte";
+  import Side from "./Side.svelte";
+
+  function color(side) {
+    return side === "B" ? "green" : "red";
+  }
+
+
 </script>
 
 <div>
-  <h3>Trades</h3>
-  <table>
-    {#each $trades as trade (trade.trade_id)}
+  <h5 class="ui dividing header">Trades</h5>
+  <table class="ui very compact very basic small table">
+    <thead>
       <tr>
-        <td>
-          <Date value={trade.trade_creation_time} />
-        </td>
-        <td>{trade.quantity}</td>
-        <td>{trade.price}</td>
-        <td>{trade.side}</td>
+        <th>Side</th>
+        <th>Quantity</th>
+        <th>Price</th>
+        <th>Time</th>
       </tr>
-    {/each}
+    </thead>
+    <tbody>
+      {#each $trades as trade (trade.trade_id)}
+        <tr>
+          <td><Side value={trade.side} /></td>
+          <td>{trade.quantity}</td>
+          <td>{trade.price}</td>
+          <td>
+            <Date value={trade.trade_creation_time} />
+          </td>
+        </tr>
+      {/each}
+    </tbody>
   </table>
 </div>
