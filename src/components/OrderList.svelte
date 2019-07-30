569917c10758017c2649eff7a0690c54ceee9b04
aurel <aurelien.catinon@gmail.com>
2019-07-30 21:48:28 +0200

    Move filled orders next to latest trades

--- src/components/OrderList.svelte
+++ src/components/OrderList.svelte
@@ -1,7 +1,6 @@
 <script>
   import { orders } from "../stores/orders";
   import { cancelOrder } from "../services/orders";
-  import { onMount } from "svelte";
   import Side from "./Side.svelte";
   import FromNow from "./FromNow.svelte";
 
@@ -9,71 +8,40 @@
     cancelOrder(orderId);
   }
 
-  onMount(async () => {
-    jQuery("#orders .menu .item").tab();
-  });
 </script>
 
-<div class="ui top attached tabular menu">
-  <span class="active item" data-tab="open">Open orders</span>
-  <span class="item" data-tab="filled">Filled orders</span>
-</div>
-<div class="ui bottom attached active tab" data-tab="open">
-  <table class="ui very compact very basic small table">
-    <thead>
+<h5 class="ui dividing header">Open orders</h5>
+<table class="ui very compact very basic small table">
+  <thead>
+    <tr>
+      <th>Side</th>
+      <th>Type</th>
+      <th>Quantity (BTC)</th>
+      <th>Price (USD)</th>
+      <th>Remaining (BTC)</th>
+      <th>Time</th>
+      <th />
+    </tr>
+  </thead>
+  <tbody>
+    {#each $orders.openOrders as order (order.id)}
       <tr>
-        <th>Side</th>
-        <th>Type</th>
-        <th>Quantity (BTC)</th>
-        <th>Price (USD)</th>
-        <th>Remaining (BTC)</th>
-        <th>Time</th>
-        <th />
+        <td>
+          <Side value={order.side} />
+        </td>
+        <td>{order.type}</td>
+        <td>{order.quantity}</td>
+        <td>{order.price}</td>
+        <td>{order.remaining_quantity}</td>
+        <td>
+          <FromNow value={order.creation_date} />
+        </td>
+        <td>
+          <a href="#" on:click|once={e => submitOrderCancellation(order.id)}>
+            <i class="red times icon" />
+          </a>
+        </td>
       </tr>
-    </thead>
-    <tbody>
-      {#each $orders.openOrders as order (order.id)}
-        <tr>
-          <td><Side value={order.side} /></td>
-          <td>{order.type}</td>
-          <td>{order.quantity}</td>
-          <td>{order.price}</td>
-          <td>{order.remaining_quantity}</td>
-          <td>
-            <FromNow value={order.creation_date} />
-          </td>
-          <td>
-            <a href="#" on:click|once={e => submitOrderCancellation(order.id)}>
-              <i class="red times icon"></i>
-            </a>
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-</div>
-<div class="ui bottom attached tab" data-tab="filled">
-  <table class="ui very compact very basic small table">
-    <thead>
-      <tr>
-        <th>Side</th>
-        <th>Price (USD)</th>
-        <th>Quantity (BTC)</th>
-        <th>Time</th>
-        <th />
-      </tr>
-    </thead>
-    <tbody>
-      {#each $orders.filledOrders as order (order.id)}
-        <tr>
-          <td><Side value={order.side} /></td>
-          <td>{order.price}</td>
-          <td>{order.quantity}</td>
-          <td>
-            <FromNow value={order.creation_date} />
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-</div>
+    {/each}
+  </tbody>
+</table>
--- src/components/TradeList.svelte
+++ src/components/TradeList.svelte
@@ -1,37 +1,76 @@
 <script>
   import { trades } from "../stores/trades";
+  import { orders } from "../stores/orders";
+  import { onMount } from "svelte";
   import Date from "./Date.svelte";
   import Side from "./Side.svelte";
+  import FromNow from "./FromNow.svelte";
 
   function color(side) {
     return side === "B" ? "green" : "red";
   }
 
-
+  onMount(async () => {
+    jQuery("#trades .menu .item").tab();
+  });
 </script>
 
 <div>
-  <h5 class="ui dividing header">Trades</h5>
-  <table class="ui very compact very basic small table">
-    <thead>
-      <tr>
-        <th>Side</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Time</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each $trades as trade (trade.trade_id)}
+  <div class="ui top attached tabular menu">
+    <span class="active item" data-tab="latest">Latest trades</span>
+    <span class="item" data-tab="my">My</span>
+  </div>
+  <div class="ui bottom attached active tab" data-tab="latest">
+    <table class="ui very compact very basic small table">
+      <thead>
         <tr>
-          <td><Side value={trade.side} /></td>
-          <td>{trade.quantity}</td>
-          <td>{trade.price}</td>
-          <td>
-            <Date value={trade.trade_creation_time} />
-          </td>
+          <th>Side</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Time</th>
         </tr>
-      {/each}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {#each $trades as trade (trade.trade_id)}
+          <tr>
+            <td>
+              <Side value={trade.side} />
+            </td>
+            <td>{trade.quantity}</td>
+            <td>{trade.price}</td>
+            <td>
+              <Date value={trade.trade_creation_time} />
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+  <div class="ui bottom attached tab" data-tab="my">
+    <table class="ui very compact very basic small table">
+      <thead>
+        <tr>
+          <th>Side</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Time</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {#each $orders.filledOrders as order (order.id)}
+          <tr>
+            <td>
+              <Side value={order.side} />
+            </td>
+            <td>{order.quantity}</td>
+            <td>{order.price}</td>
+            <td>
+              <FromNow value={order.creation_date} />
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
