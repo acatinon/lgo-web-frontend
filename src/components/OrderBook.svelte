76bb26e094b013cfcdd5ad355e41c8212ab56ca5
aurel <aurelien.catinon@gmail.com>
2019-08-01 23:28:38 +0200

    WIP: Order book refactoring

--- src/components/OrderBook.svelte
+++ src/components/OrderBook.svelte
@@ -1,47 +1,80 @@
 <script>
   import { orderBook } from "../stores/order_book";
   import { derived } from "svelte/store";
+  import { color } from "../utils/ui";
   import BigNumber from "bignumber.js";
 
   let a = new BigNumber(12);
   let b = new BigNumber(12);
 
-  const sortNumber = (a, b) => b.comparedTo(a);
+  const sortNumber = (a, b) => a.comparedTo(b);
+  const sortNumberReverse = (a, b) => b.comparedTo(a);
 
-  const sortedPrices = derived(orderBook, $orderBook => [
-    ...Array.from($orderBook.asks.keys()).sort(sortNumber),
-    ...Array.from($orderBook.bids.keys()).sort(sortNumber)
-  ]);
+  const sortedAsks = derived(orderBook, $orderBook =>
+    Array.from($orderBook.asks.keys()).sort(sortNumber)
+  );
+  const sortedBids = derived(orderBook, $orderBook =>
+    Array.from($orderBook.bids.keys()).sort(sortNumberReverse)
+  );
 
-  const combined = derived(orderBook, $orderBook => {
-    return new Map([...$orderBook.asks, ...$orderBook.bids]);
-  });
+  const combined = derived(
+    [sortedAsks, sortedBids],
+    ([$sortedAsks, $sortedBids]) => {
+      const comb = [];
+      let askSum = new BigNumber(0);
+      let bidSum = new BigNumber(0);
+      const size = Math.max($sortedAsks.length, $sortedBids.length);
+      for (let i = 0; i < size; i++) {
+        const item = { index: i };
+        item.ask = $sortedAsks[i];
+        if (item.ask) {
+          item.askQuantity = $orderBook.asks.get(item.ask);
+          item.askValue = item.ask.multipliedBy(item.askQuantity);
+          item.askSum = askSum = askSum.plus(item.askValue);
+        }
 
-  function color(price) {
-    if (price.isGreaterThanOrEqualTo($orderBook.minAsk)) {
-      return "red";
-    } else if (price.isLessThanOrEqualTo($orderBook.maxBid)) {
-      return "green";
-    } else {
-      return "";
+        item.bid = $sortedBids[i];
+        if (item.bid) {
+          item.bidQuantity = $orderBook.bids.get(item.bid);
+          item.bidValue = item.bid.multipliedBy(item.bidQuantity);
+          item.bidSum = bidSum = bidSum.plus(item.askValue);
+        }
+
+        comb.push(item);
+      }
+      return comb;
     }
-  }
+  );
 </script>
 
 <table class="ui very compact very basic small table">
   <thead>
     <tr>
-      <th class="right aligned">Quantity</th>
-      <th class="right aligned">Price</th>
+      <th class="two wide right aligned">Sum</th>
+      <th class="two wide right aligned">Value</th>
+      <th class="one wide right aligned">Quantity</th>
+      <th class="one wide right aligned">Bid</th>
+      <th class="one wide">Ask</th>
+      <th class="one wide right aligned">Quantity</th>
+      <th class="two wide right aligned">Value</th>
+      <th class="two wide right aligned">Sum</th>
     </tr>
   </thead>
   <tbody>
-    {#each $sortedPrices as price (price)}
+    {#each $combined as c (c.index)}
       <tr>
-        <td class="right aligned">{$combined.get(price).toFormat(4)}</td>
+        <td class="right aligned">{c.bidSum || ''}</td>
+        <td class="right aligned">{c.bidValue || ''}</td>
+        <td class="right aligned">{c.bidQuantity || ''}</td>
         <td class="right aligned">
-          <span class="ui {color(price)} text">{price.toFormat(8)}</span>
+          <span class="ui {color('B')} text">{c.bid || ''}</span>
         </td>
+        <td>
+          <span class="ui {color('S')} text">{c.ask || ''}</span>
+        </td>
+        <td class="right aligned">{c.askQuantity || ''}</td>
+        <td class="right aligned">{c.askValue || ''}</td>
+        <td class="right aligned">{c.askSum || ''}</td>
       </tr>
     {/each}
   </tbody>
